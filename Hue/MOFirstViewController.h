23e6e2ed3527688288848e99cf2ffecbfd0d4554
Mike Onorato <mikeono@gmail.com>
2013-03-12 22:22:54 -0400

    Initial Commit

---
+++ Hue/MOFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  MOFirstViewController.h
+//  Hue
+//
+//  Created by Mike Onorato on 3/12/13.
+//  Copyright (c) 2013 Mike Onorato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MOFirstViewController : UIViewController
+
+@end
---
+++ HueTests/HueTests.h
@@ -0,0 +1,13 @@
+//
+//  HueTests.h
+//  HueTests
+//
+//  Created by Mike Onorato on 3/12/13.
+//  Copyright (c) 2013 Mike Onorato. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HueTests : SenTestCase
+
+@end
